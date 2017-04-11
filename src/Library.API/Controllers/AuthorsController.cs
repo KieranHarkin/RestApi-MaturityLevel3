37a252817f340ef8a6112a3b1160618cd9081148
Kieran Harkin <kieranharkin@googlemail.com>
2017-04-11 13:54:23 +0100

    Setting up AutoMapper

--- src/Library.API/Controllers/AuthorsController.cs
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -1,12 +1,14 @@
-﻿using Library.API.Helpers;
+﻿using AutoMapper;
+using Library.API.Helpers;
 using Library.API.Models;
 using Library.API.Services;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 
 namespace Library.API.Controllers
 {
-    [Route("api/authors")]
+    [Route("api/[controller]")]
     public class AuthorsController : Controller
     {
         private ILibraryRepository _libraryRepository;
@@ -21,21 +23,19 @@ namespace Library.API.Controllers
         {
             var authorsFromRepo = _libraryRepository.GetAuthors();
 
-            var authors = new List<AuthorDto>();
-
-            foreach (var author in authorsFromRepo)
-            {
-                authors.Add(new AuthorDto()
-                {
-                    Id = author.Id,
-                    Name = $"{author.FirstName} {author.LastName}",
-                    Genre = author.Genre,
-                    Age = author.DateOfBirth.GetCurrentAge()
-                });
-            }
-
+            var authors = Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
 
             return new JsonResult(authors);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthor(Guid id)
+        {
+            var authorEntity = _libraryRepository.GetAuthor(id);
+
+            var authorDto = Mapper.Map<AuthorDto>(authorEntity);
+
+            return new JsonResult(authorDto);
         }
     }
 }
