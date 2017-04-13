1c09f0f057ca5c3f0eca22f2b81f3ff1f88182cc
Kieran Harkin <kieranharkin@googlemail.com>
2017-04-13 19:52:24 +0100

    Create Child Resources with a Parent Resource

--- src/Library.API/Models/AuthorForCreationDto.cs
+++ src/Library.API/Models/AuthorForCreationDto.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Library.API.Models
 {
     public class AuthorForCreationDto
     {
+        public AuthorForCreationDto()
+        {
+            Books = new List<BookForCreationDto>();
+        }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTimeOffset DateOfBirth { get; set; }
         public string Genre { get; set; }
+
+        public ICollection<BookForCreationDto> Books { get; set; }
     }
 }
