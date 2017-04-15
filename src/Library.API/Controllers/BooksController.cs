715587bb1f0dd3e525d07b99dcdfddbc7a032171
Kieran Harkin <kieranharkin@googlemail.com>
2017-04-15 14:13:53 +0100

    Delete a Resource

--- src/Library.API/Controllers/BooksController.cs
+++ src/Library.API/Controllers/BooksController.cs
@@ -82,6 +82,31 @@ namespace Library.API.Controllers
                 new { authorId = authorId, id = bookToReturn.Id }, 
                 bookToReturn);
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBookForAuthor(Guid authorId, Guid id)
+        {
+            if (!_libraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var bookForAuthor = _libraryRepository.GetBookForAuthor(authorId, id);
+
+            if (bookForAuthor == null)
+            {
+                return NotFound();
+            }
+
+            _libraryRepository.DeleteBook(bookForAuthor);
+
+            if (!_libraryRepository.Save())
+            {
+                throw new Exception($"Deleting book {id} for author {authorId} failed on save.");
+            }
+
+            return NoContent();
+        }
     }
 }
 
