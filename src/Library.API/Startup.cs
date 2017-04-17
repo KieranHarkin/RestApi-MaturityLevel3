6e104e6be7d3ae1d67e0b88f35775845d17e39a3
Kieran Harkin <kieranharkin@googlemail.com>
2017-04-18 00:35:12 +0100

    Paging Collection Resources

--- src/Library.API/Startup.cs
+++ src/Library.API/Startup.cs
@@ -10,7 +10,10 @@ using Microsoft.EntityFrameworkCore;
 using Library.API.Models;
 using Library.API.Helpers;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
 using NLog.Extensions.Logging;
 
 namespace Library.API
@@ -50,6 +53,15 @@ namespace Library.API
 
             // register the repository
             services.AddScoped<ILibraryRepository, LibraryRepository>();
+
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
+            services.AddScoped<IUrlHelper, UrlHelper>(implementationFactory =>
+            {
+                var actionContext =
+                    implementationFactory.GetService<IActionContextAccessor>().ActionContext;
+                return new UrlHelper(actionContext);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
